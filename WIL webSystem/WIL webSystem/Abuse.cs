27fbde9102362111880d266817b5d09de1abf432
Daniel <15021296@vcconnect.co.za>
2018-08-30 10:23:25 +0200

    All Classes Added DW

--- WIL webSystem/WIL webSystem/Abuse.cs
+++ WIL webSystem/WIL webSystem/Abuse.cs
@@ -7,5 +7,62 @@ namespace WIL_webSystem
 {
     public class Abuse
     {
+        public virtual int studentId
+        {
+            get;
+            set;
+        }
+
+        public virtual int guardianId
+        {
+            get;
+            set;
+        }
+
+        public virtual string description
+        {
+            get;
+            set;
+        }
+
+        public virtual string actionTaken
+        {
+            get;
+            set;
+        }
+
+        public virtual string comment
+        {
+            get;
+            set;
+        }
+
+        public virtual DateTime dateCreated
+        {
+            get;
+            set;
+        }
+
+        public virtual int id
+        {
+            get;
+            set;
+        }
+
+        public virtual int staffId
+        {
+            get;
+            set;
+        }
+
+        public virtual void recordAbuseReport(int studentId, string comment, string description, int guardianId, int staffId, string actionTaken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual Abuse viewAbuseReport(object studentId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
