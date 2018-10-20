95e9b52ec6abdd9378b009cf3736904d4f7611c8
andredev1 <andredev1234567@gmail.com>
2018-10-21 00:05:35 +0200

    resolved conflict

--- WebApplication8/GuardianSignUp.aspx.cs
+++ WebApplication8/GuardianSignUp.aspx.cs
@@ -4,23 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApplication8.BusinessLogic;
 
 namespace WebApplication8
 {
     public partial class GuardianSignUp : System.Web.UI.Page
     {
-        back.SQLConnect sqlConn ;
+        string FirstName;
+        string Surname;
+        string Password;
+        string Email;
+        string Address;
+        string ContactNo;
+        int IdNumber;
+
+
+
+        back.SQLConnect sqlConn;
         protected void Page_Load(object sender, EventArgs e)
         {
+
             sqlConn = new back.SQLConnect();
+
+
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            List<string> data = new List<string>();
+            
+            FirstName = txtFirstName.Text;
+            Surname = txtSurname.Text;
+            Password = txtPassword.Text;
+            Email = txtEmail.Text;
+            Address = txtAddress.Text;
+            ContactNo = txtContactNo.Text;
+            IdNumber = int.Parse(txtIdNumber.Text.ToString());
 
-
-
+            
+            Guardian Guard = new Guardian(IdNumber, FirstName, Surname, ContactNo, Email, Address, true, Password);
+            if(!Guard.InsertToDatabase().Equals(""))
+            {
+                 
+                lblRegisterSuccess.Text= "Error";
+            }
+                
+                
         }
 
         protected void Button2_Click(object sender, EventArgs e)
