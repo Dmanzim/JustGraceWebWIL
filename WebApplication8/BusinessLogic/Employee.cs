6a0e161bf27c8c33a148b95c7cca7790e766a6e1
Daniel <15021296@vcconnect.co.za>
2018-10-21 01:09:36 +0200

    Create Lesson code. Employee Changes

--- WebApplication8/BusinessLogic/Employee.cs
+++ WebApplication8/BusinessLogic/Employee.cs
@@ -10,7 +10,7 @@ namespace WebApplication8.BusinessLogic
 {
     public class Employee
     {
-        private int iD;
+        private int iD, employeeTypeId;
         private string fName, lName, contactNumber, email, password;
         private bool isActive;
         protected SqlConnection connection = new SqlConnection(Properties.Settings.Default.connectionStr);
@@ -25,25 +25,27 @@ namespace WebApplication8.BusinessLogic
             this.password = "";
             this.isActive = false;
         }
-        public Employee(int iD, string fName, string lName, string contactNumber, string email, string password, bool isActive)
+        public Employee(int iD, string fName, string lName, string contactNumber, string email, string password, bool isActive,int employeeTypeID)
         {
-            this.iD = ID;
+            this.ID1 = ID;
             this.fName = fName;
             this.lName = lName;
             this.contactNumber = contactNumber;
             this.email = email;
             this.password = password;
             this.isActive = isActive;
+            this.employeeTypeId = employeeTypeID;
         }
 
-        public int ID { get => iD; set => iD = value; }
+        public int ID { get => ID1; set => ID1 = value; }
         public string FName { get => fName; set => fName = value; }
         public string LName { get => lName; set => lName = value; }
         public string ContactNumber { get => contactNumber; set => contactNumber = value; }
         public string Email { get => email; set => email = value; }
         public string Password { get => password; set => password = value; }
         public bool IsActive { get => isActive; set => isActive = value; }
-
+        public int ID1 { get => iD; set => iD = value; }
+        public int EmployeeTypeId { get => employeeTypeId; set => employeeTypeId = value; }
 
         public bool loginEmployee()
         {
@@ -84,8 +86,8 @@ namespace WebApplication8.BusinessLogic
             connection.Open();
             try
             {
-                string sql = "Insert into tbl_Employee (fld_FName,fld_LName,fld_ContactNo,fld_Email,fld_Password,fld_Active) " +
-                    "values ('" + this.FName + "','" + this.LName + "','" + this.ContactNumber + "','" + this.Email + "','" + this.Password + "','" + this.IsActive + "')";
+                string sql = "Insert into tbl_Employee (fld_FName,fld_LName,fld_ContactNo,fld_Email,fld_Password,fld_Active, fld_EmployeeTypeID) " +
+                    "values ('" + this.FName + "','" + this.LName + "','" + this.ContactNumber + "','" + this.Email + "','" + this.Password + "','" + this.IsActive + "','" + this.employeeTypeId + "')";
 
                 SqlCommand cmd = new SqlCommand(sql, connection);
                 cmd.ExecuteNonQuery();
@@ -113,7 +115,8 @@ namespace WebApplication8.BusinessLogic
                     "fld_ContactNo = '" + this.ContactNumber + "'," +
                     "fld_Email = '" + this.Email + "' ," +
                     "fld_Password = '" + this.Password + "' ," +
-                    "fld_Active = '" + this.IsActive + "' ," + "' where fld_EmployeeID = '" + EmployeeID + "'";
+                    "fld_Active = '" + this.IsActive + "' ," +
+                    "fld_EmployeeTypeID = '" + this.employeeTypeId + "' ," + "' where fld_EmployeeID = '" + EmployeeID + "'";
                 SqlCommand cmd = new SqlCommand(sql, connection);
                 cmd.ExecuteNonQuery();
                 result = "";
@@ -150,6 +153,7 @@ namespace WebApplication8.BusinessLogic
                     this.Email = (dataReader.GetValue(4).ToString());
                     this.Password = (dataReader.GetValue(5).ToString());
                     this.IsActive = bool.Parse(dataReader.GetValue(6).ToString());
+                    this.employeeTypeId = int.Parse(dataReader.GetValue(7).ToString());
 
                 }
                 dataReader.Close();
@@ -171,6 +175,34 @@ namespace WebApplication8.BusinessLogic
         {
             DataTable dt = new DataTable();
             string query = "select * from tbl_Employee";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            connection.Close();
+            da.Dispose();
+
+            return dt;
+        }
+        public DataTable getEmployeeTeachersNameDataTable()
+        {
+            DataTable dt = new DataTable();
+            string query = "select fld_EmployeeID, CONCAT(FLD_FNAME, ' ',FLD_LNAME) as [Name]  from tbl_Employee where fld_EmployeeTypeID = 2";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            connection.Close();
+            da.Dispose();
+
+            return dt;
+        }
+        public DataTable getEmployeeAdminNameDataTable()
+        {
+            DataTable dt = new DataTable();
+            string query = "select fld_EmployeeID, CONCAT(FLD_FNAME, ' ',FLD_LNAME) as [Name]  from tbl_Employee where fld_EmployeeTypeID = 1 ";
 
             SqlCommand cmd = new SqlCommand(query, connection);
             connection.Open();
